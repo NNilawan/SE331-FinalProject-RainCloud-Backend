b1fec689162bce9c0ebf1dbc8d82eeaaca82c2e1
NNilawan <nilawan_l@cmu.ac.th>
2021-10-24 03:03:04 +0700

    Modify Dao Retrieve UserList For doctor and admin

--- src/main/java/se331/lab/rest/dao/UserDao.java
+++ src/main/java/se331/lab/rest/dao/UserDao.java
@@ -1,8 +1,16 @@
 package se331.lab.rest.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import se331.lab.rest.security.entity.User;
 
 public interface UserDao {
     User getUser(Long id);
+
+    Page<User> getUsersForDoctor(Long id, Pageable pageable);
+    Page<User> getUsersForDoctor(Long id, String title, Pageable pageable);
+
+    Page<User> getUsers(Integer pageSize, Integer page);
+    Page<User> getUsers(String name, Pageable page);
 
 }
--- src/main/java/se331/lab/rest/dao/UserDaoImpl.java
+++ src/main/java/se331/lab/rest/dao/UserDaoImpl.java
@@ -1,6 +1,9 @@
 package se331.lab.rest.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 import se331.lab.rest.security.entity.User;
 import se331.lab.rest.security.repository.UserRepository;
@@ -15,4 +18,23 @@ public class UserDaoImpl implements UserDao{
         return userRepository.findById(id).orElse(null);
     }
 
+    @Override
+    public Page<User> getUsersForDoctor(Long id, Pageable page) {
+        return userRepository.findByGotVaccine_doctorId(id, page);
+    }
+
+    @Override
+    public Page<User> getUsersForDoctor(Long id, String name, Pageable page) {
+        return userRepository.findByGotVaccine_doctorIdAndFirstnameIgnoreCaseContainingOrGotVaccine_doctorIdAndLastnameIgnoreCaseContaining(id, name,id,name,page);
+    }
+
+    @Override
+    public Page<User> getUsers(Integer pageSize, Integer page) {
+        return userRepository.findAll(PageRequest.of(page - 1, pageSize));
+    }
+
+    @Override
+    public Page<User> getUsers(String name, Pageable page) {
+        return userRepository.findByFirstnameIgnoreCaseContainingOrLastnameIgnoreCaseContaining(name,name,page);
+    }
 }
