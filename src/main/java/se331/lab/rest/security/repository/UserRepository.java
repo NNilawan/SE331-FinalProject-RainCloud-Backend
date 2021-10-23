cfaed091a1d87f470dee50b2ac16e1d118714e37
NNilawan <nilawan_l@cmu.ac.th>
2021-10-24 03:06:27 +0700

    Modify Repo Retrieve UserList For doctor and admin

--- src/main/java/se331/lab/rest/security/repository/UserRepository.java
+++ src/main/java/se331/lab/rest/security/repository/UserRepository.java
@@ -1,9 +1,15 @@
 package se331.lab.rest.security.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import se331.lab.rest.security.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
 
+    Page<User> findByGotVaccine_doctorId(Long doctorId, Pageable pageRequest);
+    Page<User> findByGotVaccine_doctorIdAndFirstnameIgnoreCaseContainingOrGotVaccine_doctorIdAndLastnameIgnoreCaseContaining(Long doctorId, String firstname, Long doctorId2, String lastname, Pageable pageRequest);
+
+    Page<User> findByFirstnameIgnoreCaseContainingOrLastnameIgnoreCaseContaining(String firstname, String lastname, Pageable pageRequest);
 }
