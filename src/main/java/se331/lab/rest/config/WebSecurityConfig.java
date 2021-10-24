382b2114dafe10f9520e44b0399c5c4f4606b22e
ThansudaJ21 <thansuda_janthakham.cmu.ac.th>
2021-10-24 14:47:23 +0700

    Update permission for admin

--- src/main/java/se331/lab/rest/config/WebSecurityConfig.java
+++ src/main/java/se331/lab/rest/config/WebSecurityConfig.java
@@ -67,6 +67,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.POST,"/events").hasRole("ADMIN")
                 .antMatchers(HttpMethod.POST,"/comments/{id}").hasRole("DOCTOR")
                 .antMatchers(HttpMethod.POST,"/admin/{id}").hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET,"/admin").hasRole("ADMIN")
                 .antMatchers(HttpMethod.GET,"/events").permitAll()
                 .antMatchers(HttpMethod.GET,"/organizers").permitAll()
                 .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
